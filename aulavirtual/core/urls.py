c9d12ddc6182ebbd5e52cab5ceefaf5068d81674
Nahir Jara <nahiir066@gmail.com>
2023-11-29 19:58:36 -0300

    views 2

--- aulavirtual/core/urls.py
+++ aulavirtual/core/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views #cuando pongo el punto me refiero al mismo directorio
 
 urlpatterns = [
-    path('', views.index, name='index') #url, nombre de la vista que lo atiende y un name
+    path('', views.index, name='index'), #url, nombre de la vista que lo atiende y un name
+    path('alumnos_listado', views.alumnos_listado, name='alumnos_listado'),
+    path('alumnos/detalle/<str:nombre_alumno>', views.alumno_detalle, name='alumnos_detalle'), # para pasar parametros atravez de url
+    re_path(r'alumnos/historico/(?P<year>[0-9]{4})/$', views.alumnos_historico, name='alumnos_historico'),  # para filtrar url 
+    path('alumnos/activos', views.alumnos_estado, {'estado': 'activo'},name='alumnos_estados'), # con los parametros de esta manera una misma vista puede atender dos parametros distintos
+    path('alumnos/inactivos', views.alumnos_estado, {'estado': 'inactivo'},name='alumnos_estados'),
+
 ]
--- aulavirtual/core/views.py
+++ aulavirtual/core/views.py
@@ -2,5 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+"""
 def index(request): #luego del def el nombre de la vista
-    return HttpResponse("Hola mundo parte 2")  
+    #para chequear si viene por get o post:
+    if request.method == "GET":
+        return HttpResponse("Hola mundo parte 2")
+    elif request.method == "POST":
+        return HttpResponse('')       
+"""
+def index(request):
+    return render(request, "index.html")
+
+def alumnos_listado(request):
+    return HttpResponse('Listado de alumnos:')
+
+def alumno_detalle(request, nombre_alumno):
+    return HttpResponse(
+    f"""
+    <h1>Bienvendid@ {nombre_alumno}</h1>
+    <p>Página personal de usuario</p>
+    """
+    )
+    
+def alumnos_historico(request, year):
+    return HttpResponse(f'Historico de alumnos del año: {year}')
+
+def alumnos_estado(request, estado):
+    return HttpResponse(f'El estado del alumno se encuentra {estado}')
